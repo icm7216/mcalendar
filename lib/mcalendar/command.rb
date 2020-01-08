83c45bd83a7bc2869420c0cd9f779773d389a0d2
icm7216 <icm7216d@gmail.com>
2020-01-08 16:36:45 +0900

    Add command class

--- lib/mcalendar/command.rb
+++ lib/mcalendar/command.rb
@@ -1,11 +1,26 @@
 # Ruby Monthly calendar
 
-begin
-  d = Date.parse(ARGV.first)
-rescue
-  d = Date.today
-end
+module Mcalendar
+  class Command
 
-calendar = Mcalendar::Calendar.new(d.year, d.month)
-calendar.display
-calendar.pdf
+    def self.run(argv)
+      new(argv).execute
+    end
+
+    def initialize(argv)
+      @argv = argv
+    end
+
+    def execute
+      begin
+        d = Date.parse(@argv.first)
+      rescue
+        d = Date.today
+      end
+      
+      calendar = Mcalendar::Calendar.new(d.year, d.month)
+      calendar.display
+      calendar.pdf
+    end
+  end
+end
