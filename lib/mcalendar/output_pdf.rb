414713d30f258517819bf49bf7764dc791bc0cc1
icm7216 <icm7216d@gmail.com>
2020-01-17 18:56:43 +0900

    Split day rows into upper and lower parts

--- lib/mcalendar/output_pdf.rb
+++ lib/mcalendar/output_pdf.rb
@@ -4,7 +4,7 @@ module Mcalendar
     def initialize(obj_calendar)
       @month_header = [[obj_calendar.month_title]]
       @day_of_week = [Mcalendar::Config::DAY_OF_WEEK] 
-      @calendar = obj_calendar.days.each_slice(7).to_a
+      @calendar = obj_calendar.days.each_slice(7).map
       @cell_height = @calendar.size > 5 ? 50 : 60
  
       super(page_size: 'A4', margin: 20)
@@ -27,12 +27,25 @@ module Mcalendar
         cells[0, 0].text_color = Mcalendar::Config::COLOR[:red]
       end
     end
-    
+
     def day_content
-      table(@calendar, cell_style: {height: @cell_height}) do
-        cells.style(width: 79, align: :center, size: 19)
-        row(0).padding = 15
-        column(0).map {|c| c.text_color = Mcalendar::Config::COLOR[:red]}
+      @calendar.each do |c|
+        table([c], cell_style: {height: @cell_height / 2}) do
+          cells.style(width: 79, align: :center, size: 19)
+          row(0).borders = [:top, :left, :right]
+          row(0).padding_top = 10
+          row(0).padding_bottom = 0
+          column(0).map {|c| c.text_color = Mcalendar::Config::COLOR[:red]}
+        end
+
+        dummy_text = [""] * c.size
+        table([dummy_text], cell_style: {height: @cell_height / 2}) do
+          cells.style(width: 79, align: :center, size: 10)
+          row(0).style = {overflow: :shrink_to_fit}
+          row(0).borders = [:bottom, :left, :right]
+          row(0).padding_top = 2
+          row(0).padding_bottom = 0
+        end
       end
     end
 
