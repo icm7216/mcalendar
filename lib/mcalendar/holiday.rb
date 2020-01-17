e1765ef4069e68ec99e6f0a799902b1000e5a847
icm7216 <icm7216d@gmail.com>
2020-01-17 19:02:14 +0900

    Add holiday table

---
+++ lib/mcalendar/holiday.rb
@@ -0,0 +1,22 @@
+module Mcalendar
+  HOLIDAY = {
+    "20200101": "New Year's Day", 
+    "20200113": "Coming-of-age Day", 
+    "20200211": "National Foundation Day", 
+    "20200223": "Emperor's Birthday", 
+    "20200224": "Substitute Holiday", 
+    "20200320": "Vernal Equinox Day", 
+    "20200429": "Showa Day", 
+    "20200503": "Constitution Memorial Day", 
+    "20200504": "Greenery Day", 
+    "20200505": "Children's Day", 
+    "20200506": "Substitute Holiday", 
+    "20200723": "Marine Day", 
+    "20200724": "Sports Day", 
+    "20200810": "Mountain Day", 
+    "20200921": "Respect for the Aged Day", 
+    "20200922": "Autumnal Equinox Day", 
+    "20201103": "Culture Day", 
+    "20201123": "Labour Thanksgiving Day"
+  }
+end
