b4f2dc70cd1283763fdfdcfe6bca18125afecab6
icm7216 <icm7216d@gmail.com>
2020-02-10 12:47:11 +0900

    Fix version option

--- lib/mcalendar/command.rb
+++ lib/mcalendar/command.rb
@@ -2,7 +2,6 @@
 
 module Mcalendar
   class Command
-
     def self.run(argv)
       new(argv).execute
     end
@@ -12,12 +11,12 @@ module Mcalendar
     end
 
     def execute
-
       options = Mcalendar::Options.parse(@argv)
       date = options[:date]
       console = options[:opt][:console]
       pdf = options[:opt][:pdf]
       pdf_name = options[:opt][:name]
+      version = options[:opt][:version]
       calendar = Mcalendar::Calendar.new(date.year, date.month, pdf_name)
 
       # output calendar
@@ -25,11 +24,10 @@ module Mcalendar
       calendar.pdf if pdf
 
       # both outputs if no options
-      if console.nil? && pdf.nil?
+      if console.nil? && pdf.nil? && version.nil?
         calendar.display
         calendar.pdf
       end
-      
     end
   end
 end
--- lib/mcalendar/options.rb
+++ lib/mcalendar/options.rb
@@ -1,17 +1,17 @@
 module Mcalendar
   module Options
-
     def self.parse(argv)
       options = {}
 
       parser = OptionParser.new do |o|
-        o.on_head('-v', '--version', 'Show version') do
+        o.on_head("-v", "--version", "Show version") do |v|
+          options[:version] = v
           o.version = Mcalendar::VERSION
           puts o.version
         end
-        o.on('-p', '--pdf', 'output pdf') {|v| options[:pdf] = v}
-        o.on('-n NAME', '--name=NAME', String, 'output pdf name') {|v| options[:name] = v}
-        o.on('-c', '--console', 'output console') {|v| options[:console] = v}
+        o.on("-p", "--pdf", "output pdf") { |v| options[:pdf] = v }
+        o.on("-n NAME", "--name=NAME", String, "output pdf name") { |v| options[:name] = v }
+        o.on("-c", "--console", "output console") { |v| options[:console] = v }
       end
 
       begin
@@ -24,14 +24,14 @@ module Mcalendar
           options[:name] = Mcalendar::DEFAULT_PDF_NAME
         end
       end
-      
+
       begin
         d = Date.parse(remained.first)
       rescue
         d = Date.today
       end
 
-      {date: d, opt: options}
+      { date: d, opt: options }
     end
   end
 end
