2fb7db816972d2be73d3fc5fd71b75c19feacaf6
icm7216 <icm7216d@gmail.com>
2021-09-18 03:42:32 +0900

    Update to 2021 holidays and anniversaries.

--- lib/mcalendar/holiday.rb
+++ lib/mcalendar/holiday.rb
@@ -1,63 +1,62 @@
 module Mcalendar
   module JA
     # Japanese holidays in Japanese
+    # https://www8.cao.go.jp/chosei/shukujitsu/gaiyou.html
     HOLIDAY = {
-      "20200101": "元日",
-      "20200113": "成人の日",
-      "20200211": "建国記念の日",
-      "20200223": "天皇誕生日",
-      "20200224": "振替休日",
-      "20200320": "春分の日",
-      "20200429": "昭和の日",
-      "20200503": "憲法記念日",
-      "20200504": "みどりの日",
-      "20200505": "子供の日",
-      "20200506": "振替休日",
-      "20200723": "海の日",
-      "20200724": "スポーツの日",
-      "20200810": "山の日",
-      "20200921": "敬老の日",
-      "20200922": "秋分の日",
-      "20201103": "文化の日",
-      "20201123": "勤労感謝の日",
+      "20210101": "元日",
+      "20210111": "成人の日",
+      "20210211": "建国記念の日",
+      "20210223": "天皇誕生日",
+      "20210320": "春分の日",
+      "20210429": "昭和の日",
+      "20210503": "憲法記念日",
+      "20210504": "みどりの日",
+      "20210505": "子供の日",
+      "20210722": "海の日",
+      "20210723": "スポーツの日",
+      "20210808": "山の日",
+      "20210809": "振替休日",
+      "20210920": "敬老の日",
+      "20210923": "秋分の日",
+      "20211103": "文化の日",
+      "20211123": "勤労感謝の日",
     }.freeze
 
     ANNIVERSARY = {
       "20200224": "Rubyの誕生日",
-      "20200903": "RubyKaigi 2020 Nagano",
-      "20200904": "RubyKaigi 2020 Nagano",
-      "20200905": "RubyKaigi 2020 Nagano",
+      "20200909": "RubyKaigi Takeout 2021",
+      "20200910": "RubyKaigi Takeout 2021",
+      "20200911": "RubyKaigi Takeout 2021",
     }.freeze
   end
 
   module EN
     # Japanese holidays in English
     HOLIDAY = {
-      "20200101": "New Year's Day",
-      "20200113": "Coming-of-age Day",
-      "20200211": "National Foundation Day",
-      "20200223": "Emperor's Birthday",
-      "20200224": "Substitute Holiday",
-      "20200320": "Vernal Equinox Day",
-      "20200429": "Showa Day",
-      "20200503": "Constitution Memorial Day",
-      "20200504": "Greenery Day",
-      "20200505": "Children's Day",
-      "20200506": "Substitute Holiday",
-      "20200723": "Marine Day",
-      "20200724": "Sports Day",
-      "20200810": "Mountain Day",
-      "20200921": "Respect for the Aged Day",
-      "20200922": "Autumnal Equinox Day",
-      "20201103": "Culture Day",
-      "20201123": "Labour Thanksgiving Day",
+      "20210101": "New Year's Day",
+      "20210111": "Coming-of-age Day",
+      "20210211": "National Foundation Day",
+      "20210223": "Emperor's Birthday",
+      "20210320": "Vernal Equinox Day",
+      "20210429": "Showa Day",
+      "20210503": "Constitution Memorial Day",
+      "20210504": "Greenery Day",
+      "20210505": "Children's Day",
+      "20210722": "Marine Day",
+      "20210723": "Sports Day",
+      "20210808": "Mountain Day",
+      "20210809": "Substitute Holiday",
+      "20210920": "Respect for the Aged Day",
+      "20210923": "Autumnal Equinox Day",
+      "20211103": "Culture Day",
+      "20211123": "Labour Thanksgiving Day",
     }.freeze
 
     ANNIVERSARY = {
-      "20200224": "Ruby's Birthday",
-      "20200903": "RubyKaigi 2020 Nagano",
-      "20200904": "RubyKaigi 2020 Nagano",
-      "20200905": "RubyKaigi 2020 Nagano",
+      "20210224": "Ruby's Birthday",
+      "20210909": "RubyKaigi Takeout 2021",
+      "20210910": "RubyKaigi Takeout 2021",
+      "20210911": "RubyKaigi Takeout 2021",
     }.freeze
   end
 end
